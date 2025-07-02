06dabb3b4d64b5597fc33899d03303c55d6ee3e8
gpffhajdzl <gpffhajdzl@naver.com>
2025-07-02 13:31:08 +0900

    초기 React + Vite + TailwindCSS 프로젝트 설정

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* Header */}
+      <header className="header">
+        <h1 className="findora-title">Findora</h1>
+      </header>
+
+      {/* Main Content */}
+      <main className="main-content">
+        {/* 메인 컨텐츠 영역 */}
+      </main>
+
+      {/* Footer */}
+      <footer className="footer">
+        <p>&copy; 2024 Findora. All rights reserved.</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
